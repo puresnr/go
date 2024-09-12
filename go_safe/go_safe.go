9529459da75b856d12cf8846e3bf9c0ea4839240
YangYang <yangyang@inke.cn>
2024-09-12 13:20:28 +0800

    go_safe && mem_cache

---
+++ go_safe/go_safe.go
@@ -0,0 +1,97 @@
+// package gosafe 提供一个安全的方式来开启协程, 当 panic 时, 不会导致进程退出, 调用方式: 直接把待执行函数传进去即可, 比如Go(f(){})
+package go_safe
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"os"
+	"runtime"
+)
+
+func PrintPanicStack(extras ...interface{}) {
+	i := 0
+	funcName, file, line, ok := runtime.Caller(i)
+	for ok {
+		fmt.Fprintf(os.Stderr, "frame %v:[func:%v,file:%v,line:%v]\n", i, runtime.FuncForPC(funcName).Name(), file, line)
+		i++
+		funcName, file, line, ok = runtime.Caller(i)
+	}
+	for k := range extras {
+		fmt.Fprintf(os.Stderr, "EXTRAS#%v DATA:%v\n", k, spew.Sdump(extras[k]))
+	}
+}
+
+func onExit(extras ...interface{}) {
+	if err := recover(); err != nil {
+		PrintPanicStack(extras)
+		fmt.Fprintf(os.Stderr, "<recover from panic: %s>\n", err)
+	}
+}
+
+// Go 用于执行无入参的函数, panic 时不会开启新的协程重新执行该函数
+// 入参:
+//
+//	f: 待执行函数
+func Go(f func()) {
+	go func() {
+		defer onExit()
+
+		f()
+	}()
+}
+
+// GoP 用于执行有入参的函数, panic 时不会开启新的协程重新执行该函数
+// 入参:
+//
+//	f: 待执行函数
+//	p: 待执行函数使用的入参, 如果入参数量多于 1 个, 需要把所有入参包装为一个结构体, 使用该结构体作为入参
+func GoP[T any](f func(T), p T) {
+	go func() {
+		defer onExit()
+
+		f(p)
+	}()
+}
+
+func onExitR(f func(), extras ...interface{}) {
+	if err := recover(); err != nil {
+		PrintPanicStack(extras)
+		fmt.Fprintf(os.Stderr, "<recover from panic: %s>\n", err)
+
+		GoR(f)
+	}
+}
+
+// GoR 用于执行无入参的函数, panic 时会开启新的协程重新执行该函数
+// 入参:
+//
+//	f: 待执行函数
+func GoR(f func()) {
+	go func() {
+		defer onExitR(f)
+
+		f()
+	}()
+}
+
+func onExitPR[T any](f func(T), p T, extras ...interface{}) {
+	if err := recover(); err != nil {
+		PrintPanicStack(extras)
+		fmt.Fprintf(os.Stderr, "<recover from panic: %s>\n", err)
+
+		GoPR(f, p)
+	}
+}
+
+// GoPR 用于执行有入参的函数, panic 时会开启新的协程重新执行该函数
+// 入参:
+//
+//	f: 待执行函数
+//	p: 待执行函数使用的入参, 如果入参数量多于 1 个, 需要把所有入参包装为一个结构体, 使用该结构体作为入参
+func GoPR[T any](f func(T), p T) {
+	go func() {
+		defer onExitPR(f, p)
+
+		f(p)
+	}()
+}
---
+++ mem_cache/mem_cache.go
@@ -0,0 +1,67 @@
+package mem_cache
+
+import (
+	"context"
+	"github.com/puresnr/pgo/go_safe"
+	"sync"
+	"time"
+)
+
+type memCache[K comparable, V any] struct {
+	locker       *sync.RWMutex
+	cache        map[K]V
+	funcNewCache func(context.Context, K) (V, error)
+}
+
+func (m *memCache[K, V]) Get(ctx context.Context, key K) (V, error) {
+	m.locker.RLock()
+	v, ok := m.cache[key]
+	m.locker.RUnlock()
+	if ok {
+		return v, nil
+	}
+
+	var err error
+	if v, err = m.funcNewCache(ctx, key); err != nil {
+		return v, err
+	}
+
+	m.locker.Lock()
+	m.cache[key] = v
+	m.locker.Unlock()
+
+	return v, nil
+}
+
+func (m *memCache[K, V]) del() {
+	m.locker.Lock()
+	c, dc := 0, len(m.cache)/3
+	if c != dc {
+		for k, _ := range m.cache {
+			delete(m.cache, k)
+			c++
+			if c == dc {
+				break
+			}
+		}
+	}
+	m.locker.Unlock()
+}
+
+func New[K comparable, V any](f func(context.Context, K) (V, error)) *memCache[K, V] {
+	mc := &memCache[K, V]{locker: new(sync.RWMutex), cache: make(map[K]V), funcNewCache: f}
+
+	go_safe.GoR(func() {
+		ticker := time.NewTicker(86400 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				mc.del()
+			}
+		}
+	})
+
+	return mc
+}
