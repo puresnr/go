25b646e21e1acb4b5be94aa7e29a40ee64d53de3
puresnr <768751338@163.com>
2025-09-22 15:57:22 +0800

    prand: support return value

--- prand/rand.go
+++ prand/rand.go
@@ -15,17 +15,64 @@ var (
 	ErrorInvalidSumProbs = errors.New("sum probs should be 1")
 	ErrorInvalidProbs    = errors.New("invalid probs")
 	ErrorNegativeProb    = errors.New("probability cannot be negative")
+	ErrorMismatchedLen   = errors.New("values and probs must have the same length")
 )
 
-// Probs is a cumulative probability distribution.
+// WeightedRand is a cumulative probability distribution.
 // It is an opaque type and can only be created by NewProbs.
-type Probs struct {
+type WeightedRand[T any] struct {
+	values     []T
 	cumulative []float64
+}
+
+// NewProbs creates a cumulative probability distribution from a slice of raw probabilities.
+// The sum of rawprobs must be equal to 1.0 within a tolerance of Epsilon.
+// All probabilities in rawprobs must be non-negative.
+func NewWeightedRand[T any](values []T, rawprobs []float64) (*WeightedRand[T], error) {
+	if len(values) != len(rawprobs) {
+		return nil, ErrorMismatchedLen
+	}
+
+	lenProbs := len(rawprobs)
+
+	if lenProbs == 0 {
+		return nil, ErrorEmptyProbs
+	}
+
+	if rawprobs[0] < 0 {
+		return nil, ErrorNegativeProb
+	}
+	wr := &WeightedRand[T]{values: values, cumulative: make([]float64, lenProbs)}
+	wr.cumulative[0] = rawprobs[0]
+	for i := 1; i < lenProbs; i++ {
+		if rawprobs[i] < 0 {
+			return nil, ErrorNegativeProb
+		}
+		wr.cumulative[i] = wr.cumulative[i-1] + rawprobs[i]
+	}
+
+	if math.Abs(wr.cumulative[lenProbs-1]-1.0) > Epsilon {
+		return nil, ErrorInvalidSumProbs
+	}
+
+	// Force the last element to be exactly 1.0 to guarantee the invariant
+	// and simplify the logic in RandIdx.
+	wr.cumulative[lenProbs-1] = 1.0
+
+	return wr, nil
+}
+
+func (p *WeightedRand[T]) Rand() (v T, err error) {
+	idx, err := p.RandIdx()
+	if err != nil {
+		return
+	}
+	return p.values[idx], nil
 }
 
 // RandIdx returns a random index based on the cumulative probability distribution.
 // It uses binary search and has a time complexity of O(log N).
-func (p *Probs) RandIdx() (int, error) {
+func (p *WeightedRand[T]) RandIdx() (int, error) {
 	if p == nil || len(p.cumulative) == 0 {
 		return 0, ErrorInvalidProbs
 	}
@@ -37,37 +84,4 @@ func (p *Probs) RandIdx() (int, error) {
 	// Using ">" instead of ">=" is crucial for correctly handling values
 	// that fall on the boundaries between buckets.
 	return sort.Search(len(p.cumulative), func(i int) bool { return p.cumulative[i] > value }), nil
-}
-
-// NewProbs creates a cumulative probability distribution from a slice of raw probabilities.
-// The sum of rawprobs must be equal to 1.0 within a tolerance of Epsilon.
-// All probabilities in rawprobs must be non-negative.
-func NewProbs(rawprobs []float64) (*Probs, error) {
-	lenProbs := len(rawprobs)
-
-	if lenProbs == 0 {
-		return nil, ErrorEmptyProbs
-	}
-
-	if rawprobs[0] < 0 {
-		return nil, ErrorNegativeProb
-	}
-	probs := &Probs{cumulative: make([]float64, lenProbs)}
-	probs.cumulative[0] = rawprobs[0]
-	for i := 1; i < lenProbs; i++ {
-		if rawprobs[i] < 0 {
-			return nil, ErrorNegativeProb
-		}
-		probs.cumulative[i] = probs.cumulative[i-1] + rawprobs[i]
-	}
-
-	if math.Abs(probs.cumulative[lenProbs-1]-1.0) > Epsilon {
-		return nil, ErrorInvalidSumProbs
-	}
-
-	// Force the last element to be exactly 1.0 to guarantee the invariant
-	// and simplify the logic in RandIdx.
-	probs.cumulative[lenProbs-1] = 1.0
-
-	return probs, nil
 }
