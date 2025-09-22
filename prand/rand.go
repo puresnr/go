e3d69b1f0a0988a16e2470b0e59413268309b542
puresnr <768751338@163.com>
2025-09-22 11:38:39 +0800

    feat(prand): add probability-based random index selection

---
+++ prand/rand.go
@@ -0,0 +1,53 @@
+package prand
+
+import (
+	"errors"
+	"math/rand/v2"
+)
+
+var (
+	ErrorEmptyProbs      = errors.New("empty probs")
+	ErrorInvalidSumProbs = errors.New("sum probs should be 1")
+	ErrorInvalidProbs    = errors.New("invalid probs")
+)
+
+type Probs []float64
+
+func (p Probs) RandIdx() (int, error) {
+	if lenp := len(p); lenp == 0 || p[lenp-1] != 1 {
+		return 0, ErrorInvalidProbs
+	}
+
+	value := rand.Float64()
+
+	for idx, v := range p {
+		if value < v {
+			return idx, nil
+		}
+	}
+
+	// should not be reached
+	return 0, ErrorInvalidProbs
+}
+
+func NewProbs(rawprobs []float64) (Probs, error) {
+	lenProbs := len(rawprobs)
+
+	if lenProbs == 0 {
+		return nil, ErrorEmptyProbs
+	}
+
+	probs := make(Probs, 0, lenProbs)
+	probs = append(probs, rawprobs[0])
+	if lenProbs > 1 {
+		for i := 1; i != lenProbs; i++ {
+			probs = append(probs, probs[i-1] + rawprobs[i])
+		}
+	}
+
+	if probs[lenProbs-1] != 1 {
+		return nil, ErrorInvalidSumProbs
+	}
+	return probs, nil
+}
+
