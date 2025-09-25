3a5f72503fc0317f589a3573739f7f270e9ce175
puresnr <768751338@163.com>
2025-09-25 16:43:32 +0800

    deepcopy: slice for basic type

---
+++ deepcopy/slice.go
@@ -0,0 +1,26 @@
+package deepcopy
+
+import "github.com/puresnr/go/algo"
+
+// Basic is a constraint that permits any of Go's basic types.
+// This is used to ensure that Clone is not used with reference types
+// where it would only perform a shallow copy.
+type Basic interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+	~float32 | ~float64 |
+	~string | ~bool | ~complex64 | ~complex128
+}
+
+// Clone creates a deep copy of a slice of basic data types.
+func Deepcopy[T Basic](s []T) []T {
+	// If the original slice is nil, return nil to maintain that state.
+	if algo.Empty_slice(s) {
+		return nil
+	}
+
+	// Create a new slice and copy the elements from the old slice.
+	newSlice := make([]T, len(s))
+	copy(newSlice, s)
+	return newSlice
+}
--- deepcopy/slice_test.go
+++ deepcopy/slice_test.go
@@ -1,4 +1,4 @@
-package slice
+package deepcopy
 
 import (
 	"reflect"
