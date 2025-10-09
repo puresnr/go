8666c8ab2ca2ef78923b7bbdd04b00660148bab8
puresnr <768751338@163.com>
2025-10-09 12:19:02 +0800

    note

--- perror/ecode/ecode.go
+++ perror/ecode/ecode.go
@@ -2,6 +2,12 @@ package ecode
 
 import "errors"
 
+// Ecode represents an error type identified by a string code.
+// It is designed to work with the standard errors.Is function.
+// Two Ecode instances are considered equal by errors.Is if they are both of type Ecode
+// and were created with the same code string.
+// Ecode is intended to be a source of errors, not a wrapper around other errors,
+// so it does not implement the Unwrap method.
 type Ecode struct{ error }
 
 func New(ecode string) *Ecode { return &Ecode{error: errors.New(ecode)} }
