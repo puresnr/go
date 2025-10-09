6cb32f6f3a8060290e3d93ac5284a816c09fdf6d
puresnr <768751338@163.com>
2025-10-09 12:15:17 +0800

    perror

--- perror/ecode/ecode.go
+++ perror/ecode/ecode.go
@@ -5,3 +5,14 @@ import "errors"
 type Ecode struct{ error }
 
 func New(ecode string) *Ecode { return &Ecode{error: errors.New(ecode)} }
+
+func (e *Ecode) Is(target error) bool {
+	if target == nil {return e == nil}
+	
+	et, ok := target.(*Ecode)
+	if !ok {
+		return false
+	}
+
+	return e.Error() == et.Error()
+}
